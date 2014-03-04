e622f7441ba6b336deff37b8b44844d180addfb1
Ian Melnick <melnick@gmail.com>
2014-03-03 22:57:59 -0600

    Split out logic into class

--- app/main.php
+++ app/main.php
@@ -8,12 +8,23 @@ $app = new \Slim\Slim(array(
     'log.level' => \Slim\Log::DEBUG,
 ));
 
+$app->response->headers->set('Content-Type', 'application/json');
+
+
+class ApiV1 {
+
+    function hello($name) {
+        return $this->render("Hello, $name");
+    }
+
+    private function render($data) {
+        echo json_encode($data);
+    }
+
+}
+
 $app->group('/v1', function () use ($app) {
-
-    $app->get('/hello/:name', function ($name) {
-        echo "Hello, $name";
-    });
-
+    $app->get('/hello/:name', '\Memoia\SpartzFun\ApiV1:hello');
 });
 
 $app->run();
