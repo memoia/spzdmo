18d2a47e97e6f067a9a1b7adf505fb8ff612ce6c
Ian Melnick <melnick@gmail.com>
2014-03-05 22:30:19 -0600

    Add citiesNear route; handle deliberate exceptions
    * :warning: The bounding box or its usage appears to be very wrong

--- app/main.php
+++ app/main.php
@@ -3,19 +3,28 @@ namespace Memoia\SpzDmo;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 require 'db.php';
+require 'util.php';
+require 'exception.php';
 require 'autoload.php';
 spl_autoload_register('\Memoia\SpzDmo\api_version_loader');
 
 
 $app = new \Slim\Slim(array(
-    'debug' => true,
+    'debug' => false,
     'log.enabled' => true,
     'log.level' => \Slim\Log::DEBUG,
 ));
 
+$app->error(function (\Exception $err) use ($app) {
+    if ($err instanceof Exceptions\ValidationError) {
+        $app->halt(500, $err->getMessage());
+    }
+});
+
 $app->group('/v1', function () use ($app) {
     $app->get('/hello/:name', '\Memoia\SpzDmo\V1\Api:hello');
     $app->get('/states/:state/cities.json', '\Memoia\SpzDmo\V1\Api:cities');
+    $app->get('/states/:state/cities/:cityName.json', '\Memoia\SpzDmo\V1\Api:citiesNear');
 });
 
 $app->run();
--- app/v1.php
+++ app/v1.php
@@ -1,6 +1,9 @@
 <?php
 namespace Memoia\SpzDmo\V1;
 
+use Memoia\SpzDmo\Util;
+use Memoia\SpzDmo\Exceptions;
+
 class Api
 {
     public function hello($name)
@@ -15,6 +18,35 @@ class Api
                 ->where('state', $state)
                 ->find_array()
         );
+    }
+
+    public function citiesNear($state, $cityName)
+    {
+        $radius = \Slim\Slim::getInstance()->request->params('radius');
+        if (! (is_null($radius) || is_numeric($radius))) {
+            throw new Exceptions\ValidationError('Radius querystring param is required and must be a number');
+        }
+
+        $city = \ORM::for_table('cities')
+                    ->where('state', $state)
+                    ->where('name', $cityName)
+                    ->where('status', 'verified')
+                    ->find_one();
+        if ($city === false) {
+            throw new Exceptions\ValidationError('Requested city is unknown');
+        }
+        $bounds = Util\bounding_box($radius, $city->latitude, $city->longitude);
+        $in_box = \ORM::for_table('cities')
+                      ->where_lte('latitude', $bounds['max_lat'])
+                      ->where_gte('latitude', $bounds['min_lat'])
+                      ->where_lte('longitude', $bounds['max_lon'])
+                      ->where_lte('longitude', $bounds['min_lon'])
+                      ->find_array();
+
+        $this->render($in_box);     // XXX something is really wrong here
+
+        // TODO maybe further filter $in_box result like shown in
+        // the reference as sql...
     }
 
     private function render($data)
