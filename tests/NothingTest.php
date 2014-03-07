87aa7f46132cf358213edbdf2e9b58813cc7cd19
Ian Melnick <melnick@gmail.com>
2014-03-06 22:59:31 -0600

    Show how to set up goofy test db in test classes...
    PHPUnit does have a database helper, and it looks like
    Phinx also has some kind of database test thing, but
    this will do for now....

--- tests/NothingTest.php
+++ tests/NothingTest.php
@@ -3,6 +3,12 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 class NothingTest extends PHPUnit_Framework_TestCase
 {
+    public static function setUpBeforeClass()
+    {
+        require dirname(__DIR__).'/app/db.php';
+        \ORM::configure('sqlite:'.dirname(__DIR__).'/data/test.sqlite3');
+    }
+
     public function testNothing()
     {
         $this->assertTrue(true);
